68c6ca809b8eeeea9d6c3efb44a863661b983fef
lahirukawijes <lahiruka.wijesinghe@gmail.com>
2019-08-28 08:31:15 +0530

    impl chatobservable

--- ChatRoom/src/publicChatRoom/ChatObservable.java
+++ ChatRoom/src/publicChatRoom/ChatObservable.java
@@ -1,5 +1,7 @@
 package publicChatRoom;
 
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +13,21 @@ package publicChatRoom;
  * @author Lahiruka
  */
 public class ChatObservable {
-    
+    private String message="";
+    private ArrayList<ChatObserver> chatObserverList=new ArrayList<>();
+    public void addChatObserver(ChatObserver chatObserver){
+        chatObserverList.add(chatObserver);
+    }
+    public void removeChatObserver(ChatObserver chatObserver){
+        chatObserverList.remove(chatObserver);
+    }
+    public void notifyChatObservers(){
+        for (ChatObserver chatObserver : chatObserverList) {
+            chatObserver.update(message);
+        }
+    }
+    public void setMessage(String message){
+        this.message=message;
+        notifyChatObservers();
+    }
 }
